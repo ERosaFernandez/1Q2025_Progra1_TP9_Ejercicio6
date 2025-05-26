84a78117aa7a977531b4e5999b1daf64af2d846f
Enzorofer <erosafernandez@itba.edu.ar>
2025-05-25 21:10:46 -0300

    Agregado de comentarios

--- Input.h
+++ Input.h
@@ -1,15 +1,21 @@
 #ifndef INPUT_H_
 #define INPUT_H_
 
-//algo para actualizar el eclipse >:(
 
 enum results {
 	SALIR = -5, ENCENDER, LIMPIAR, CONMUTAR
 };
+//Lista de posibles resultados de la funcion input.
+//Cada uno esta asociado a un numero
 
 typedef int8_t flag;
+//asignacion al flag como una variable de 8 bit
 
 int8_t input ();
+//funcion input: recibe caracteres para determinar
+//la tarea a realizar
 void instrucciones();
+//funcion instrucciones: imprime por pantalla
+//una serie de referencias
 
 #endif /* INPUT_H_ */
