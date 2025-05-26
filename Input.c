af297c929fa70beb5b1dbae32ef971298e8ecb77
Enzorofer <erosafernandez@itba.edu.ar>
2025-05-25 21:10:33 -0300

    Adicion de comentarios. Modificado la funcion instrucciones

--- Input.c
+++ Input.c
@@ -2,24 +2,31 @@
 #include <stdint.h>
 #include "Input.h"
 
-//algo para actualizar el eclipse >:(
 
 #define ERRORCHECK (estado = ((input = getchar()) != '\n' && input != 0)? ERROR : estado)
+//Macro para chequear mas de un caracter presente en el buffer
 
 enum flags {
 	ERROR, OK, TOGGLE, CLEAR, SET, QUIT
 };
-
+//lista de aliases con sus numeros asociados. no son necesarios
+//para el usuario asi que quedan aca
 
 int8_t input() {	//Basado en la funcion getciclos del ejercicio 14 del TP4
 	int16_t input;
 	int8_t num;
 	flag estado = ERROR;
+	//3 variables , para determinar el input, el numero a devolver
+	// y un flag de control
 
-	instrucciones ();
+	instrucciones();
+	//Lista de referencias para el usuario
 
 	while (estado == ERROR) {
 		switch (input = getchar()) {
+		//Chequea el input de la entrada
+		//Segun lo recibido, hace una determinada asignacion
+		//al numero y al flag
 		case 't':
 		case 'T':
 			estado = TOGGLE;
@@ -46,18 +53,24 @@ int8_t input() {	//Basado en la funcion getciclos del ejercicio 14 del TP4
 					num = (input - '0');
 					estado = OK;
 					ERRORCHECK;
-			}
-			}
-			else estado = ERROR;
+				}
+			} else
+				estado = ERROR;
 		}
-		if (estado == ERROR){
-			while(getchar()!='\n');
+		if (estado == ERROR) {
+			//En caso de error, resetea el numero y vuelve a pedir
+			//un caracter
+			if (input != '\n') {
+				while (getchar() != '\n');
+			}
 			printf("Valor invalido, ingrese nuevamente:\n");
 			instrucciones();
 			num = 0;
 		}
 	}
-	switch (estado){
+	switch (estado) {
+	//Segun el flag, modifica el numero a devolver
+	//sera usado para el main
 	case QUIT:
 		num = SALIR;
 		break;
@@ -73,15 +86,15 @@ int8_t input() {	//Basado en la funcion getciclos del ejercicio 14 del TP4
 	return num;
 }
 
-void instrucciones (){
-	printf("**********Referencia**********\n");
-	printf("*                            *\n");
-	printf("*0-7: Nro. de led a encender *\n");
-	printf("*t,T: Conmutar todos los leds*\n");
-	printf("*c,C: Apagar todos los leds  *\n");
-	printf("*s,S: Encender todos los leds*\n");
-	printf("*q,Q: Salir                  *\n");
-	printf("******************************\n");
+void instrucciones() {
+	printf("\n***********Referencia***********\n");
+	printf("*                              *\n");
+	printf("* 0-7: Nro. de led a encender  *\n");
+	printf("* t,T: Conmutar todos los leds *\n");
+	printf("* c,C: Apagar todos los leds   *\n");
+	printf("* s,S: Encender todos los leds *\n");
+	printf("* q,Q: Salir                   *\n");
+	printf("*                              *\n");
+	printf("********************************\n");
 }
-
 
